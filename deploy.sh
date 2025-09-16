de3deffadff997e7230dfe8daa443d6c4497a392
a-s-t-e-y-a <krishnoit100@gmail.com>
2025-09-16 15:57:28 +0530

    ♿️ Improve shell file include piston in this file also

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+git clone https://github.com/engineer-man/piston.git ../piston
+( cd ../piston && docker-compose up -d api && cd cli && npm i )
+../piston/cli/index.js ppman install javascript
+../piston/cli/index.js ppman install python
 pnpm install
 docker-compose up -d
 pnpm run db:generate
